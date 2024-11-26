c3f37ce6989efeea84f19316bfabadb75fefb44e
Malav Treasurer <malav.treasurer@gmail.com>
2024-11-26 20:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import socket
 from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 #    print request.headers
-    return "Hello World from Canary app!"
+    return "Hello World from Canary app!" + "\n" + "Pod name is: " + socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
